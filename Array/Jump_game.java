6764557cd6d9bf12c9350b1bc72f5c41798226dc
jash2212 <70933400+jash2212@users.noreply.github.com>
2021-08-13 12:15:43 +0530

    Create Jump_game.java

---
+++ Array/Jump_game.java
@@ -0,0 +1,53 @@
+// { Driver Code Starts
+
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            int N = Integer.parseInt(read.readLine());
+            
+            String S1[] = read.readLine().split(" ");
+            
+            int[] A = new int[N];
+            
+            for(int i=0; i<N; i++)
+                A[i] = Integer.parseInt(S1[i]);
+
+            Solution ob = new Solution();
+            System.out.println(ob.canReach(A,N));
+        }
+    }
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    static int canReach(int[] A, int N) {
+        int max=0;
+        for(int i=0;i<A.length;i++)
+        {
+            if(max>=i)
+            {
+                 max=Math.max(max,A[i]+i); 
+                //System.out.println(max);    
+            }
+            else
+            {
+                return 0;
+            }
+           
+        }
+        if(max>=N-1)
+        {
+            return 1;
+        }
+        return 0;
+    }
+};
