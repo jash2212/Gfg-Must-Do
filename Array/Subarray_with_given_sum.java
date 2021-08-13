25e5081c6e64015a812ec5d0bf76387c0730c23d
jash2212 <70933400+jash2212@users.noreply.github.com>
2021-08-13 12:02:40 +0530

    Create Subarray_with_given_sum.java

---
+++ Array/Subarray_with_given_sum.java
@@ -0,0 +1,74 @@
+// { Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main{
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        for (int i = 0; i < t; i++) {
+            int n = sc.nextInt();
+            int s = sc.nextInt();
+
+            int[] m = new int[n];
+            for (int j = 0; j < n; j++) {
+                m[j] = sc.nextInt();
+            }
+            
+            Solution obj = new Solution();
+            ArrayList<Integer> res = obj.subarraySum(m, n, s);
+            for(int ii = 0;ii<res.size();ii++)
+                System.out.print(res.get(ii) + " ");
+            System.out.println();
+        }
+    }
+
+}// } Driver Code Ends
+
+
+
+class Solution
+{
+    //Function to find a continuous sub-array which adds up to a given number.
+    static ArrayList<Integer> subarraySum(int[] arr, int n, int s) 
+    {
+        // Your code here
+        ArrayList<Integer> a=new ArrayList<Integer>();
+        int i=0;
+        int j=0;
+        int sum=0;
+       
+            while(j<arr.length)
+            {
+                if(sum<s)
+                {
+                    sum=sum+arr[j];
+                    
+                    j++;
+                    
+                }
+                else if(sum>s)
+                {
+                   
+                        sum=sum-arr[i];
+                       
+                        i++;
+                    
+                }
+                if(sum==s)
+                {
+                    a.add(i+1);
+                    a.add(j);
+                    return a;
+                }
+               
+                
+            }
+            
+        a.add(-1);
+        return a;
+    }
+}
