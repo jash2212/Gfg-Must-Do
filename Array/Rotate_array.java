482f5e6feb664a7bbc52c50346f7a3136ba1a644
jash2212 <70933400+jash2212@users.noreply.github.com>
2021-08-13 12:08:09 +0530

    Create Rotate_array.java

---
+++ Array/Rotate_array.java
@@ -0,0 +1,46 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+
+
+void rotateArr(int arr[], int d, int n){
+    // code here
+}
+
+
+
+
+// { Driver Code Starts.
+
+int main() {
+	int t;
+	//taking testcases
+	cin >> t;
+	
+	while(t--){
+	    int n, d;
+	    
+	    //input n and d
+	    cin >> n >> d;
+	    
+	    int arr[n];
+	    
+	    //inserting elements in the array
+	    for(int i = 0; i < n; i++){
+	        cin >> arr[i];
+	    }
+	    
+	    //calling rotateArr() function
+	    rotateArr(arr, d,n);
+	    
+	    //printing the elements of the array
+	    for(int i =0;i<n;i++){
+	        cout << arr[i] << " ";
+	    }
+	    cout << endl;
+	}
+	return 0;
+}  // } Driver Code Ends
